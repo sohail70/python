d82907ac9fe3cbbfab55c6eeece603fd813dea57
sohail70 <sohail.e.nia@gmail.com>
2021-01-18 00:09:07 +0330

    3rd

--- 1.py
+++ 1.py
@@ -1,3 +1,25 @@
 item1 = "apple"
 item2 = "orange"
-print(item1,item2)
+item3 = "banana"
+item4 = "peach"
+item5 = "cherries"
+print(item1,item2,item3,item4,item5)
+
+
+cost1 = 9.42
+cost2 = 5.67
+cost3 = 3.25
+cost4 = 13.40
+cost5 = 7.50
+
+cardinality1 =1
+cardinality2 =1
+cardinality3 =1
+cardinality4 =1
+cardinality5 =5
+wholeCost = (cardinality1 * cost1
+            +cardinality2 * cost2 
+            +cardinality3 * cost3
+            +cardinality4 * cost4
+            +cardinality5 * cost5)
+print(wholeCost)
